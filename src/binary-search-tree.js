5dd2966ebeb85ec8c09c5940142c87b3a6514e67
Olga Dementey <dementeyolya@gmail.com>
2023-12-19 21:00:28 +0300

    feat: start to implement bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -3,14 +3,16 @@ const { NotImplementedError } = require('../extensions/index.js');
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root;
   }
 
   add(/* data */) {
@@ -45,5 +47,5 @@ class BinarySearchTree {
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
